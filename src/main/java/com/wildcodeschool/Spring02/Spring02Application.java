d8d9c3db550a195fa627096e05442b495cdc05d2
Ots S <53628328+Ots-S@users.noreply.github.com>
2019-10-07 16:34:50 +0200

    Update Spring02Application.java

--- src/main/java/com/wildcodeschool/Spring02/Spring02Application.java
+++ src/main/java/com/wildcodeschool/Spring02/Spring02Application.java
@@ -22,26 +22,25 @@ public class Spring02Application {
     @ResponseBody
     public String doctor(@PathVariable int number) {
         if (number == 9) {
-			return "number: " + number + " name : Christopher Eccleston";
+		return "number: " + number + " name : Christopher Eccleston";
 		}
         if (number == 10) {
-			return "number: " + number + " name : David Tennant";
+		return "number: " + number + " name : David Tennant";
 		}
         if (number == 11) {
-			return "number: " + number + " name : Matt Smith";
-
+		return "number: " + number + " name : Matt Smith";
 		}
         if (number == 12) {
-			return "number: " + number + " name : Peter Capaldi";
+		return "number: " + number + " name : Peter Capaldi";
 		}
         if (number == 13) {
-			return "number: " + number + " name : Jodie Whittaker";
+		return "number: " + number + " name : Jodie Whittaker";
 		}
 	if (number > 0 && number < 9) {
-			throw new ResponseStatusException(HttpStatus.SEE_OTHER, "303 See Other.");
+		throw new ResponseStatusException(HttpStatus.SEE_OTHER, "303 See Other.");
 		}
 	else {
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible de récupérer l'incarnation " + number );
+		throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Impossible de récupérer l'incarnation " + number );
 		}
     }
 }
